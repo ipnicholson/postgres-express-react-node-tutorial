fbdafde7d0134ec72488061668883b24e0723fb8
Ian Nicholson <ipnicholson@gmail.com>
2018-08-20 15:38:45 -0700

    disallow null values for todoItem content

---
+++ server/migrations/20180820222428-create-todo-item.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('TodoItems', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      complete: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('TodoItems');
+  }
+};

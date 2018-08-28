7629148ddd12c3cb1bea61c55369320cbe2e4770
Ian Nicholson <ipnicholson@gmail.com>
2018-08-28 15:25:04 -0700

    add GET route for todos, POST for todoItems

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 // All our routes go here
 
 const todosController = require('../controllers').todos;
+const todoItemsController = require('../controllers').todoItems;
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
@@ -9,4 +10,9 @@ module.exports = (app) => {
 
   // Run todosController.create, extract the posted data, and create a todo from it
   app.post('/api/todos', todosController.create);
+
+  // GET on api/todos => list all todos
+  app.get('/api/todos', todosController.list);
+
+  app.post('/api/todos/:todoId/items', todoItemsController.create);
 };

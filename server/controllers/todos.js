743d53a31901a67b69900463563f2eef9da6ddc6
Ian Nicholson <ipnicholson@gmail.com>
2018-09-05 11:17:40 -0700

    add retrieve method for single todo

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -12,6 +12,7 @@ module.exports = {
       .catch(error => res.status(400).send(error));
   },
 
+  // list todoitems inside todos
   list(req, res) {
     return Todo
       .findAll({
@@ -21,6 +22,26 @@ module.exports = {
         }],
       })
       .then(todos => res.status(200).send(todos))
+      .catch(error => res.status(400).send(error));
+  },
+
+  // find todo whose id matches todoId from request params. Include the todoItems. Then, we send it back in the response. If no such todo exists, error object is sent back
+  retrieve(req, res) {
+    return Todo
+      .findById(req.params.todoId, {
+        include: [{
+          model: TodoItem,
+          as: 'todoItems',
+        }],
+      })
+      .then(todo => {
+        if (!todo) {
+          return res.status(404).send({
+            message: 'Todo Not Found',
+          });
+        }
+        return res.status(200).send(todo);
+      })
       .catch(error => res.status(400).send(error));
   },
 };
